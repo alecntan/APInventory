cfa140f82bffe3031ec3a7e346fe29d22785f58e
Alec Tan <alec@mars.alectan.dev>
2022-01-26 15:10:39 +1100

    cleaned up configuration files

--- apinventory/config.py
+++ apinventory/config.py
@@ -5,4 +5,3 @@ class Default:
     DATABASE_PATH='/tmp/test.db'
     SQLALCHEMY_DATABASE_URI='sqlite:///{}'.format(DATABASE_PATH)
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    TESTING=True
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 from apinventory import create_app, db
 
-app=create_app(testing=False)
+app=create_app(config_filename='prod_config.py')
 
-# If it does not exist - create
+# If db does not exist - create
 with app.app_context():
 
     if not os.path.isfile(app.config['DATABASE_PATH']):
